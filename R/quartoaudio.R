f2b16b5064db8c68ab04ce41f1307fafc3fbcee4
Quynh Nguyen <qtn2@case.edu>
2024-05-27 05:09:34 -0400

    modify all quartoaudio functions

--- R/quartoaudio.R
+++ R/quartoaudio.R
@@ -1,76 +1,65 @@
 #'convert a quarto file to audio
-#'@param key API key
-#'@param api TTS APIs (openAI, voiceRSS,)
-#'@export language language for the audio file
+#'@param api TTS APIs (openAI, voiceRSS, play.ht)
 
-source("R/quartoaudio_openAI.R")
-source("R/quartoaudio_voiceRSS.R")
-source("R/quartoaudio_playht.R")
-quartoaudio <- function (input) {
-  #scanning the files to get quarto files.
+# source("R/quartoaudio_openAI.R")
+# source("R/quartoaudio_voiceRSS.R")
+# source("R/quartoaudio_playht.R")
 
-  #choosing API
-  message("choose an API (openAI, voiceRSS, play.ht)")
-  repeat {
-    # message("choose an API (openAI, voiceRSS, play.ht)")
-    api_choice <- tolower(readline(prompt = "Enter API's name (or type 'exit' to quit): "))
-    supported_api <- c("openai", "voicerss", "play.ht")
+quartoaudio <- function (api,...) {
+  api_choice <- tolower(api)
+  args <- list(...)
 
-    if (api_choice == 'exit') {
-      return(invisible())
+  if (api == "openai") {
+    #temporary hold
+    if (!"input" %in% names(args) || is.null(args$input) || !"api_key" %in% names(args) || is.null(args$api_key)) {
+      stop("OpenAI API requires 'input' and 'api_key'")
     }
 
-    if (api_choice %in% supported_api) {
-      break
-    } else {
-      message("Not in supported API . Please try again!")
-    }
+    #default values for required params
+    if (!"model"%in% names(args)) args$model <- "tts-1"
+    if (!"voice" %in% names(args)) args$voice <- "alloy"
+    quartoaudio_openAI(input = args$input, api_key = args$api_key, model = args$model, voice = args$voice)
   }
 
-  if (api_choice == "openai") {
-    api_key <- readline(prompt = "Enter API's key: ")
-    model <- readline(prompt = "Enter model's choice (default: 'tts-1'): ")
-    voice <- readline(prompt = "Enter voice's choice (default: 'alloy'): ")
-
-    if (model == "") {
-      model <- 'tts-1'
+  else if (api == "voicerss") {
+    #temporary hold
+    if (!"input" %in% names(args)
+        || is.null(args$input)
+        || !"api_key" %in% names(args)
+        || is.null(args$api_key)) {
+      stop("VoiceRSS requires 'input' and 'api_key'")
     }
 
-    if (voice == "") {
-      voice <- "alloy"
-    }
-
-    quartoaudio_openAI(input = input, api_key = api_key, model = model, voice = voice)
-
-  } else if (api_choice == "voicerss") {
-    api_key <- readline(prompt = "Enter API's key: ")
-    language <- readline(prompt = "Enter language (default: 'en-us'): ")
-    voices <- readline(prompt = "Enter voice (default: 'Linda'): ")
-    audio_codecs <- readline(prompt = "Enter audio codecs (default: 'MP3'): ")
-    audio_format <- readline(prompt = "Enter audio format (default: '44khz_16bit_stereo'): ")
-
-    if (language == "") {
-      language <- "en-us"
-    }
-
-    if (voices == "") {
-      voices <- "Linda"
-    }
-
-    if (audio_codecs == "") {
-      audio_codecs <- "MP3"
-    }
-
-    if (audio_format == "") {
-      audio_format <- "44khz_16bit_stereo"
-    }
-
-    quartoaudio_voiceRSS(input, api_key = api_key, hl=language, c=audio_codecs, f=audio_format)
-
-  } else if (api_choice == "play.ht") {
-    user_id <- readline(prompt = "Enter UserID: ")
-    secret_key <- readline(prompt = "Enter secret key: ")
-    quartoaudio_playht(input, user_id = user_id, secret_key = secret_key)
+    #default values for required params
+    if (!"hl"%in% names(args)) args$hl <- "en-us"
+    if (!"c"%in% names(args)) args$c <- "MP3"
+    if (!"f"%in% names(args)) args$f <- "44khz_16bit_stereo"
+    quartoaudio_voiceRSS(input = args$input, api_key = args$api_key, hl=args$hl, c=args$c, f=args$f)
   }
 
+  else if (api=="play.ht") {
+    #temporary hold
+    if (!"input" %in% names(args)
+        || is.null(args$input)) {
+      stop("VoiceRSS requires 'input' ")
+    }
+
+    if (!"user_id" %in% names(args)
+        || is.null(args$user_id)) {
+      stop("VoiceRSS requires 'user_id' ")
+    }
+
+    if (!"api_key" %in% names(args)
+        || is.null(args$api_key)) {
+      stop("VoiceRSS requires 'api_key' ")
+    }
+
+    if (!"voice"%in% names(args)) args$voice <- "s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json"
+
+    quartoaudio_playht(input = args$input, user_id = args$user_id, secret_key = args$api_key, voice = args$voice)
+  }
+
+  else {
+    stop("API is not supported")
+  }
 }
--- R/quartoaudio_playht.R
+++ R/quartoaudio_playht.R
@@ -1,9 +1,13 @@
-#' Convert  a quarto file to audio using openAI API
+#' Convert  a quarto file to audio using play.ht API (https://docs.play.ht/reference/api-generate-audio)
 #' @param input The text to generate into audio
+#' @param voice the unique ID for a playHT or cloned voice
 #' @export
 
-quartoaudio_playht <- function(input, user_id, secret_key,...) {
-  library(httr)
+quartoaudio_playht <- function(input,
+                               user_id,
+                               secret_key,
+                               voice = "s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json",
+                               ...) {
   if (is.na(user_id) || is.null(user.id) || user.id == "") {
     stop("No user id is provided. Please try again!")
   }
@@ -14,25 +18,58 @@ quartoaudio_playht <- function(input, user_id, secret_key,...) {
 
   ENDPOINT <- "https://api.play.ht/api/v2/tts"
 
-  headers <-  c(
+  headers <-  list(
     `AUTHORIZATION` = secret_key,
     `X-USER-ID` = user_id,
-    `Content-Type` = "application/json"
+    `Content-Type` = "application/json",
+    `Accept` = "text/event-stream"
   )
 
   payload <- list(
     text = input,
-    voice = "s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json"
+    voice = voice
   )
 
   other_information <- list(...)
+
   payload <- c(payload, other_information)
 
-  response <- POST(ENDPOINT,
-                   body = payload,
-                   add_headers(.headers = headers),
-                   content_type("application/json"),
-                   accept("text/event-stream"),
-                   encode = "json")
-  content(response, "text")
+  request <-  httr2::request(ENDPOINT) |> httr2::req_headers(!!!headers) |> httr2::req_body_json(payload)
+
+  response <- request |> httr2::req_perform()
 }
+
+# quartoaudio_playht <- function(input, user_id, secret_key,...) {
+#   library(httr)
+#   if (is.na(user_id) || is.null(user_id) || user_id == "") {
+#     stop("No user id is provided. Please try again!")
+#   }
+#
+#   if(is.na(secret_key) || is.null(secret_key) || secret_key == "") {
+#     stop("No key is provided. Please try again!")
+#   }
+#
+#   ENDPOINT <- "https://api.play.ht/api/v2/tts"
+#
+#   headers <-  c(
+#     `AUTHORIZATION` = secret_key,
+#     `X-USER-ID` = user_id,
+#     `Content-Type` = "application/json"
+#   )
+#
+#   payload <- list(
+#     text = input,
+#     voice = "s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json"
+#   )
+#
+#   other_information <- list(...)
+#   payload <- c(payload, other_information)
+#
+#   response <- POST(ENDPOINT,
+#                    body = payload,
+#                    add_headers(.headers = headers),
+#                    content_type("application/json"),
+#                    accept("text/event-stream"),
+#                    encode = "json")
+#   content(response, "text")
+# }
